e7045dfc5b77852dc599d1ed593babe294c7968d
Hari Prasanth M M <56171545+hariprasanthmath@users.noreply.github.com>
2023-02-16 10:22:00 -0800

    Time: 28 ms (54.46%), Space: 43 MB (35.80%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String longestPalindrome(String questionStr) {
+        int questionLength = questionStr.length();
+        
+        String longest = "";
+        int longestResult = 0;
+        StringBuffer currentString = new StringBuffer(questionStr);
+            
+        for(int index=0; index<questionStr.length(); index++){
+            
+            
+            
+            
+                 int left = index;
+                 int right = index;
+                 while(left>=0 && right < questionStr.length() && questionStr.charAt(left) == questionStr.charAt(right)){
+                int currentSize = right - left + 1;
+                if(currentSize > longestResult){
+                    longest = currentString.substring(left,right+1);
+                    longestResult = currentSize;
+                }
+                left--;
+                right++;
+            }
+                
+          
+                
+                 left = index;
+                 right = index+1;
+                 while(left>=0 && right < questionStr.length() && questionStr.charAt(left) == questionStr.charAt(right)){
+                int currentSize = right - left + 1;
+                if(currentSize > longestResult){
+                    longest = currentString.substring(left,right+1);
+                    longestResult = currentSize;
+                }
+                left--;
+                right++;
+            }
+            
+        }
+        
+        
+        if(longestResult == 0){
+            return "" + questionStr.charAt(0);
+        }else{
+            return longest;
+        }
+        
+    }
+}
